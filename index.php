dff30075521bfd6b1f7749575f45dd953a8f5d78
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-05-31 16:18:29 +0200

    creazione classe e aggiunta istanze

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
+    class movie{
+        public $title;
+        public $lenguage;
+    }
+
+    $batman = new movie();
+    $batman->title = 'Batman';
+    $batman->lenguage = 'English';
+
+    var_dump($batman);
+
+    $spider_man = new movie();
+    $spider_man->title = 'Spider Man';
+    $spider_man->lenguage = 'German';
+
+    var_dump($spider_man)
+
 ?>
